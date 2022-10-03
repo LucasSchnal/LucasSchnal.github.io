0f975bf1f330ba43e6526e49bcae4007cd432404
LucasSchnal <113069315+LucasSchnal@users.noreply.github.com>
2022-10-03 11:23:23 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,19 @@
+alert ("Bienvenido a nuestra página")
+let edad
+edad = prompt ("Introduce tu edad")
+if (edad>=18){
+    alert("Puede entrar, eres mayor de edad")
+}
+else{
+    alert("Eres menor de edad, entras bajo tu propia responsabilidad")
+}
+
+alert 
+let ciudad
+ciudad = prompt ("Ingrese en que ciudad vive (Use mayúsculas al inicio)")
+if (ciudad == "San nicolas de los arroyos" || ciudad == "Ramallo" || ciudad == "Rojo"){
+    alert("Tienes envío gratis")
+}
+else{
+    alert("El costo de envío es de $800")
+}
